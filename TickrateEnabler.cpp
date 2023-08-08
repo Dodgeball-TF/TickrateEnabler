ce2dfbd69c60795410b77c9461e0de72c8eadac4
Matthew <matteus12122@gmail.com>
2023-08-08 16:47:35 +0200

    Update TickrateEnabler.cpp

--- TickrateEnabler.cpp
+++ TickrateEnabler.cpp
@@ -1,100 +1,81 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
-//
-// Purpose:
-//
-// $NoKeywords: $
-//
-//===========================================================================//
-
-#include <stdio.h>
-#include <sourcehook/sourcehook_impl.h>
-#include <eiface.h>
-#include <tier0/icommandline.h>
-
-class TickrateEnabler: public IServerPluginCallbacks
+#include "eiface.h"
+#include "sourcehook/sourcehook_impl.h"
+class TickrateEnabler : public IServerPluginCallbacks
 {
 public:
-	TickrateEnabler() {}
-	~TickrateEnabler() {}
+	TickrateEnabler(){};
+	~TickrateEnabler(){};
 
+	// IServerPluginCallbacks methods
 	virtual bool Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory);
 	virtual void Unload(void);
-	virtual void Pause(void) {}
-	virtual void UnPause(void) {}
-	virtual const char *GetPluginDescription(void)
-	{
-		return "Tickrate Enabler 0.5, updated, original by Didrole";
-	}
-	virtual void LevelInit(char const *pMapName) {}
-	virtual void ServerActivate(edict_t *pEdictList, int edictCount, int clientMax) {}
-	virtual void GameFrame(bool simulating) {}
-	virtual void LevelShutdown(void) {}
-	virtual void ClientActive(edict_t *pEntity) {}
-	virtual void ClientDisconnect(edict_t *pEntity) {}
-	virtual void ClientPutInServer(edict_t *pEntity, char const *playername) {}
-	virtual void SetCommandClient(int index) {}
-	virtual void ClientSettingsChanged(edict_t *pEdict) {}
-	virtual PLUGIN_RESULT ClientConnect(bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen)
-	{
-		return PLUGIN_CONTINUE;
-	}
-	virtual PLUGIN_RESULT ClientCommand(edict_t *pEntity, const CCommand &args)
-	{
-		return PLUGIN_CONTINUE;
-	}
-	virtual PLUGIN_RESULT NetworkIDValidated(const char *pszUserName, const char *pszNetworkID)
-	{
-		return PLUGIN_CONTINUE;
-	}
-	virtual void OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue) {}
-	virtual void OnEdictAllocated(edict_t *edict) {}
-	virtual void OnEdictFreed(const edict_t *edict) {}
-};
+	virtual void Pause(void){};
+	virtual void UnPause(void){};
+	virtual const char *GetPluginDescription(void);
+	virtual void LevelInit(char const *pMapName){};
+	virtual void ServerActivate(edict_t *pEdictList, int edictCount, int clientMax){};
+	virtual void GameFrame(bool simulating){};
+	virtual void LevelShutdown(void){};
+	virtual void ClientActive(edict_t *pEntity){};
+	virtual void ClientFullyConnect(edict_t *pEntity){};
+	virtual void ClientDisconnect(edict_t *pEntity){};
+	virtual void ClientPutInServer(edict_t *pEntity, char const *playername){};
+	virtual void SetCommandClient(int index){};
+	virtual void ClientSettingsChanged(edict_t *pEdict){};
+	virtual PLUGIN_RESULT ClientConnect(bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen) { return PLUGIN_CONTINUE; };
+	virtual PLUGIN_RESULT ClientCommand(edict_t *pEntity, const CCommand &args) { return PLUGIN_CONTINUE; };
+	virtual PLUGIN_RESULT NetworkIDValidated(const char *pszUserName, const char *pszNetworkID) { return PLUGIN_CONTINUE; };
+	virtual void OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue){};
 
-IServerGameDLL *server = nullptr;
+	// added with version 3 of the interface.
+	virtual void OnEdictAllocated(edict_t *edict){};
+	virtual void OnEdictFreed(const edict_t *edict){};
+
+	// added with version 4 of the interface.
+	virtual bool BNetworkCryptKeyCheckRequired(uint32 unFromIP, uint16 usFromPort, uint32 unAccountIdProvidedByClient, bool bClientWantsToUseCryptKey) { return false; };
+	virtual bool BNetworkCryptKeyValidate(uint32 unFromIP, uint16 usFromPort, uint32 unAccountIdProvidedByClient, int nEncryptionKeyIndexFromClient, int numEncryptedBytesFromClient, byte *pbEncryptedBufferFromClient, byte *pbPlainTextKeyForNetchan) { return false; };
+};
+// No idea why this is required for linux.
+ICvar *g_pCVar = NULL;
+
+TickrateEnabler g_EmptyServerPlugin;
+EXPOSE_SINGLE_INTERFACE_GLOBALVAR(TickrateEnabler, IServerPluginCallbacks, INTERFACEVERSION_ISERVERPLUGINCALLBACKS, g_EmptyServerPlugin);
+
+// SourceHook Embedding
 SourceHook::Impl::CSourceHookImpl g_SourceHook;
 SourceHook::ISourceHook *g_SHPtr = &g_SourceHook;
 int g_PLID = 0;
 
-TickrateEnabler g_TickrateEnabler;
-EXPOSE_SINGLE_INTERFACE_GLOBALVAR(TickrateEnabler, IServerPluginCallbacks, INTERFACEVERSION_ISERVERPLUGINCALLBACKS, g_TickrateEnabler);
-
 SH_DECL_HOOK0(IServerGameDLL, GetTickInterval, const, 0, float);
+
+IServerGameDLL *gamedll = NULL;
 
 float GetTickInterval()
 {
-#ifndef TICK_INTERVAL
-	float tickinterval = DEFAULT_TICK_INTERVAL;
-
-	if (CommandLine()->CheckParm("-tickrate"))
-	{
-		float tickrate = CommandLine()->ParmValue("-tickrate", 0.0f);
-		if (tickrate > 10)
-			tickinterval = 1.0f / tickrate;
-	}
-
+	float tickinterval = 0.01f;
 	RETURN_META_VALUE(MRES_SUPERCEDE, tickinterval);
-#else
-	RETURN_META_VALUE(MRES_SUPERCEDE, 1.0f / TICK_INTERVAL);
-#endif
+}
+
+const char *TickrateEnabler::GetPluginDescription(void)
+{
+	return "TickrateEnabler for TF2";
 }
 
 bool TickrateEnabler::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory)
 {
-	server = reinterpret_cast<IServerGameDLL*>(gameServerFactory("ServerGameDLL010", nullptr));
-
-	if (!server)
+	gamedll = (IServerGameDLL *)gameServerFactory("ServerGameDLL012", NULL);
+	if (!gamedll)
 	{
-		Warning("Failed to get a pointer on ServerGameDLL.");
+		Warning("Failed to get a pointer on ServerGameDLL.\n");
 		return false;
 	}
 
-	SH_ADD_HOOK(IServerGameDLL, GetTickInterval, server, SH_STATIC(GetTickInterval), false);
+	SH_ADD_HOOK(IServerGameDLL, GetTickInterval, gamedll, SH_STATIC(GetTickInterval), false);
 
 	return true;
 }
 
 void TickrateEnabler::Unload(void)
 {
-	SH_REMOVE_HOOK(IServerGameDLL, GetTickInterval, server, SH_STATIC(GetTickInterval), false);
+	SH_REMOVE_HOOK(IServerGameDLL, GetTickInterval, gamedll, SH_STATIC(GetTickInterval), false);
 }
